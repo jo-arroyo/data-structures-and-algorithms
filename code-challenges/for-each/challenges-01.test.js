23b61b39e1229a9ce98636548d8df1d46101c146
joannaarroyo <joliuac@gmail.com>
2019-06-04 12:13:53 -0700

    completed challenge 5

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -93,7 +93,17 @@ This anonymous function should accept up to three arguments: the element, the in
 ------------------------------------------------------------------------------------------------ */
 
 const removeWithAnon = (arr) => {
-  // Solution code here...
+  arr.forEach(element => {
+    let anon = (element, arr) => {
+      if (element % 3 === 2){
+        arr.pop();
+      }
+      return arr;
+    };
+    anon(element, arr);
+    return arr;
+  });
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -173,7 +183,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should remove three elements from the array', () => {
     expect(removeWithAnon([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).toStrictEqual([1, 2, 3, 4, 5, 6, 7]);
     expect(removeWithAnon([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]).length).toStrictEqual(7);
