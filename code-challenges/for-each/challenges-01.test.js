01182b827fb4eaa820eb2c89c03310f0aba92927
joannaarroyo <joliuac@gmail.com>
2019-06-04 12:54:42 -0700

    completed challenge 7. all done!

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -148,7 +148,19 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  let outPut = [];
+  arr.forEach(num => {
+    if (num % 3 === 0 && num % 5 === 0){
+      outPut.push('Fizz Buzz');
+    } else if (num % 5 === 0){
+      outPut.push('Buzz');
+    } else if (num % 3 === 0){
+      outPut.push('Fizz');
+    } else {
+      outPut.push(num);
+    }
+  });
+  return outPut;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -205,7 +217,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
