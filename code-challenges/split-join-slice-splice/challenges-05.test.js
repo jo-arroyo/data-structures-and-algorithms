b730ecd2ba2021b1c4b91cbc4239f4a0a0b569ec
joannaarroyo <joliuac@gmail.com>
2019-06-08 17:31:35 -0700

    completed challenge 2

--- code-challenges/split-join-slice-splice/challenges-05.test.js
+++ code-challenges/split-join-slice-splice/challenges-05.test.js
@@ -30,7 +30,11 @@ For example, wordsToCharList('gregor') returns ['g','r','e','g','o','r'].
 ------------------------------------------------------------------------------------------------ */
 
 const wordsToCharList = (arr) => {
-  // Solution code here...
+  let result = [];
+  for (var i = 0; i < arr.length; i++){
+    result.push(arr.slice(i, i+1));
+  }
+  return result;
 };
 
 
@@ -207,7 +211,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array of individual letters', () => {
     expect(wordsToCharList('Gregor')).toStrictEqual(['G', 'r', 'e', 'g', 'o', 'r']);
     expect(wordsToCharList('Gregor').length).toStrictEqual(6);
