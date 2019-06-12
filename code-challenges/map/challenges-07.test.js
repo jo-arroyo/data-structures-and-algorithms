c175760f7d61685cfec1ea9579d4d595d75ecf1e
joannaarroyo <joliuac@gmail.com>
2019-06-11 18:05:51 -0700

    completed challenge 6

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -119,7 +119,7 @@ const snorlaxAbilities = {
 };
 
 const extractAbilities = (arr) => {
-  // Solution code here...
+  return arr.map(element => element.ability.name)
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -242,7 +242,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should return an array containing only the ability names', () => {
     expect(extractAbilities(snorlaxAbilities.abilities)).toStrictEqual(['gluttony', 'cute charm', 'immunity']);
     expect(extractAbilities(snorlaxAbilities.abilities).length).toStrictEqual(3);
