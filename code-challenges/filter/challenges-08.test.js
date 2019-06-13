28f3303cdc7541d38029f97e3759b9b9d44e9945
joannaarroyo <joliuac@gmail.com>
2019-06-12 19:55:22 -0700

    completed challenge 3

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -39,7 +39,12 @@ For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
 
 const notInFirstArray = (forbiddenValues, arr) => {
-  // Solution code here...
+  let valuesNotInFirstArray = arr.filter(element => {
+    if (!forbiddenValues.includes(element)){
+      return element;
+    }
+  })
+  return valuesNotInFirstArray;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -195,7 +200,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   const firstNums = [1, 2, 3];
   const secondNums = [1, 2, 3, 4];
 
