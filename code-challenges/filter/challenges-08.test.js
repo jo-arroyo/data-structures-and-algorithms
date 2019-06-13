dc00769bff109a4a6e9a651d537ed1db42c061de
joannaarroyo <joliuac@gmail.com>
2019-06-12 19:38:16 -0700

    completed challenge 2

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -25,7 +25,8 @@ For example, filterStringsWithVowels('gregor','hound','xyz') returns ['gregor', 
 
 
 const filterStringsWithVowels = (arr) => {
-  // Solution code here...
+  let wordsWithVowels = arr.filter(word => word.match(/[aeiou]/));
+  return wordsWithVowels;
 };
 
 
@@ -181,7 +182,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array containing only words that have vowels', () => {
     expect(filterStringsWithVowels(['gregor','hound','xyz'])).toStrictEqual(['gregor', 'hound']);
     expect(filterStringsWithVowels(['gregor','hound','xyz']).length).toStrictEqual(2);
