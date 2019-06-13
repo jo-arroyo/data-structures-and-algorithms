6487c8984e8ca4c2fcc0c09380473a07b7f73d06
joannaarroyo <joliuac@gmail.com>
2019-06-12 20:42:19 -0700

    completed challenge 4

--- code-challenges/filter/challenges-08.test.js
+++ code-challenges/filter/challenges-08.test.js
@@ -87,7 +87,11 @@ const snorlaxData = {
 };
 
 const getBaseStatGreaterThan = (arr, minBaseStat) => {
-  // Solution code here...
+  let baseStats = arr.filter(element => {
+    if (element.baseStat > minBaseStat)
+      return element;
+  })
+  return baseStats;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -224,7 +228,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should return an array containing the stats that are greater than the input', () => {
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75)).toStrictEqual([ { stat: { url: 'https://pokeapi.co/api/v2/stat/5/', name: 'special-defense' }, effort: 2, baseStat: 110 } ]);
     expect(getBaseStatGreaterThan(snorlaxData.stats, 75).length).toStrictEqual(1);
