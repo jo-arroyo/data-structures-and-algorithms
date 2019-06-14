5759046b2d4c0580ded8acfd465a97540ecec6a3
joannaarroyo <joliuac@gmail.com>
2019-06-13 23:18:03 -0700

    completed challenge 7

--- code-challenges/reduce/challenges-09.test.js
+++ code-challenges/reduce/challenges-09.test.js
@@ -239,7 +239,13 @@ const snorlaxData = {
 };
 
 const extractStat = (statName, arr) => {
-  // Solution code here...
+  let thing = arr.reduce((acc, val) => {
+    if (val.stat.name === statName){
+      return acc = val;
+    }
+    return acc;
+  }, {});
+  return thing;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -303,7 +309,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   test('It should return any stats that match the input', () => {
     expect(extractStat('speed', snorlaxData.stats)).toStrictEqual({ stat: { url: 'https://pokeapi.co/api/v2/stat/6/', name: 'speed' }, effort: 5, baseStat: 30 });
   });
