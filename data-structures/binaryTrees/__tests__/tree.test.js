c02b29293f8d9b602c07b8f6b0411cab40ab1a7c
joannaarroyo <joliuac@gmail.com>
2019-09-30 22:19:03 -0700

    corrected tests done in own time

--- data-structures/binaryTrees/__tests__/tree.test.js
+++ data-structures/binaryTrees/__tests__/tree.test.js
@@ -3,7 +3,7 @@
 const Node = require('../node');
 const {Tree, findMaxValue, findHeight, invertTree} = require('../tree');
 
-xdescribe('Find Max Value in Binary Tree', () => {
+describe('Find Max Value in Binary Tree', () => {
   test('Returns null if tree is empty', () => {
     const testTree = new Tree();
     expect(findMaxValue(testTree.root)).toEqual(null);
@@ -35,7 +35,7 @@ xdescribe('Find Max Value in Binary Tree', () => {
   });
 });
 
-xdescribe('Find height of binary tree', () => {
+describe('Find height of binary tree', () => {
   test('Returns null for empty tree', () => {
     const testTree = new Tree();
     expect(findHeight(testTree.root)).toEqual(null);
@@ -74,23 +74,23 @@ xdescribe('Find height of binary tree', () => {
   });
 });
 
-//TODO Not sure if something is wrong with this test?
-describe('Invert a binary tree', () => {
-  test('Returns inverted binary tree', () => {
-    const testTree = new Tree();
-    testTree.root = new Node(1);
-    testTree.root.left = new Node(2);
-    testTree.root.left.left = new Node(3);
-    testTree.root.left.right = new Node(4);
-    testTree.root.right = new Node(5);
-    testTree.root.right.left = new Node(6);
-    testTree.root.right.right = new Node(7);
-    expect(invertTree(testTree.root.value)).toEqual(1);
-    expect(invertTree(testTree.root.left.value)).toEqual(5);
-    expect(invertTree(testTree.root.right.value)).toEqual(2);
-    expect(invertTree(testTree.root.left.left.value)).toEqual(7);
-    expect(invertTree(testTree.root.left.right.value)).toEqual(6);
-    expect(invertTree(testTree.root.right.left.value)).toEqual(4);
-    expect(invertTree(testTree.root.right.right.value)).toEqual(3);
-  });
-});
+// //TODO Not sure if something is wrong with this test?
+// xdescribe('Invert a binary tree', () => {
+//   test('Returns inverted binary tree', () => {
+//     const testTree = new Tree();
+//     testTree.root = new Node(1);
+//     testTree.root.left = new Node(2);
+//     testTree.root.left.left = new Node(3);
+//     testTree.root.left.right = new Node(4);
+//     testTree.root.right = new Node(5);
+//     testTree.root.right.left = new Node(6);
+//     testTree.root.right.right = new Node(7);
+//     expect(invertTree(testTree.root.value)).toEqual(1);
+//     expect(invertTree(testTree.root.left.value)).toEqual(5);
+//     expect(invertTree(testTree.root.right.value)).toEqual(2);
+//     expect(invertTree(testTree.root.left.left.value)).toEqual(7);
+//     expect(invertTree(testTree.root.left.right.value)).toEqual(6);
+//     expect(invertTree(testTree.root.right.left.value)).toEqual(4);
+//     expect(invertTree(testTree.root.right.right.value)).toEqual(3);
+//   });
+// });
